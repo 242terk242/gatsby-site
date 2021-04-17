e22c7318dd4fcba8c8f8bdb6bd519f1af94274c8
242terk242 <242terk242@gmail.com>
2021-04-16 21:27:54 -0500

    date update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
   <Layout>
-    <h1>4/16/2020 Updates</h1>
+    <h1>4/16/2021 Updates</h1>
     <p>
       Load pages study
-<br/><br/>
+<br/><br/>gi
       More technical than style testing going on
     </p>
     
